6aaa6ad794d8851f80d3c1ea178dde717663698b
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-29 14:51:29 -0300

    Inserir cadastrarLead na LeadService

--- src/main/java/dtos/LeadController.java
+++ src/main/java/dtos/LeadController.java
@@ -1,2 +1,22 @@
-package dtos;public class LeadController {
+package dtos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/leads")
+
+public class LeadController {
+
+    @Autowired
+    private LeadService leadService;
+
+    @PostMapping(HttpStatus.CREATED)
+    public void cadastarLead(@RequestBody LeadDto leadDto) {
+        leadService.cadastrarLead(leadDto);
+    }
 }
