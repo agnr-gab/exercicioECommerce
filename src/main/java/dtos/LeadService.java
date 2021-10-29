77dae968949290effef87c535cb228cbe9df71cd
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-29 13:58:34 -0300

    Criar metodo verificarEmail Classe LeadService

--- src/main/java/dtos/LeadService.java
+++ src/main/java/dtos/LeadService.java
@@ -1,14 +1,17 @@
 package dtos;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LeadService {
-    private List<ProdutoDto> listaProdutos = new ArrayList<>();
-    private List<LeadDto> listaLeads = new ArrayList<>();
+    private static List<ProdutoDto> listaProdutos = new ArrayList<>();
+    private static List<LeadDto> listaLeads = new ArrayList<>();
 
     public void cadastrarProduto(ProdutoDto produtoDto) {
         listaProdutos.add(produtoDto);
@@ -16,6 +19,16 @@ public class LeadService {
 
     public void cadastrarLead(LeadDto leadDto) {
         listaLeads.add(leadDto);
+    }
+
+    public static void verificarEmail(String email) {
+        for (LeadDto referencia : listaLeads) {
+            if (referencia.getEmail().equalsIgnoreCase(email)) {
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY,
+                        "Esse email já foi cadastrado! Tente novamente!");
+            }
+        }
+
     }
 
 
