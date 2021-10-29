c68b716718cd32751786ea0c11b9d58a2bc0f2d3
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-29 12:24:13 -0300

    Criar metodo cadastrarLead Classe LeadService

--- src/main/java/dtos/LeadService.java
+++ src/main/java/dtos/LeadService.java
@@ -8,8 +8,15 @@ import java.util.List;
 @Service
 public class LeadService {
     private List<ProdutoDto> listaProdutos = new ArrayList<>();
+    private List<LeadDto> listaLeads = new ArrayList<>();
 
     public void cadastrarProduto(ProdutoDto produtoDto) {
         listaProdutos.add(produtoDto);
     }
+
+    public void cadastrarLead(LeadDto leadDto) {
+        listaLeads.add(leadDto);
+    }
+
+
 }
