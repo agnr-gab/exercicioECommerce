fb0ef3b7d2ba6a127d756ece2b70723493d64d45
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-29 15:12:42 -0300

    Inserir metodo de cadastrar produto na leadController

--- src/main/java/dtos/LeadController.java
+++ src/main/java/dtos/LeadController.java
@@ -2,10 +2,7 @@ package dtos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/leads")
@@ -15,8 +12,20 @@ public class LeadController {
     @Autowired
     private LeadService leadService;
 
-    @PostMapping(HttpStatus.CREATED)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void cadastarLead(@RequestBody LeadDto leadDto) {
         leadService.cadastrarLead(leadDto);
     }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarProduto(@RequestBody ProdutoDto produtoDto) {
+        leadService.cadastrarProduto(produtoDto);
+    }
+
+
+
+
 }
