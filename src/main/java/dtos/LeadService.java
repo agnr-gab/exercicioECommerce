92a4b2383e1cf31cec12ac6c53ed9f3e416f98d6
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-29 12:18:59 -0300

    Criar metodo cadastrarProduto Classe LeadService

--- src/main/java/dtos/LeadService.java
+++ src/main/java/dtos/LeadService.java
@@ -1,4 +1,15 @@
 package dtos;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class LeadService {
+    private List<ProdutoDto> listaProdutos = new ArrayList<>();
+
+    public void cadastrarProduto(ProdutoDto produtoDto) {
+        listaProdutos.add(produtoDto);
+    }
 }
